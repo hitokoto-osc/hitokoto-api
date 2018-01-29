9ec8a1609690ea59bb58da31dfc056e1666794f6
a632079 <a632079@qq.com>
2018-01-29 19:04:33 +0800

    update utils

--- src/utils.js
+++ src/utils.js
@@ -62,24 +62,24 @@ utils.JSONtoHTML = (json, options) => {
     json = JSON.parse(json)
     json = JSON.stringify(json)
   }
-  reg = /([{}])/g
+  reg = /([\{\}])/g
   json = json.replace(reg, '\r\n$1\r\n')
-  reg = /([[]])/g
+  reg = /([\[\]])/g
   json = json.replace(reg, '\r\n$1\r\n')
-  reg = /(,)/g
+  reg = /(\,)/g
   json = json.replace(reg, '$1\r\n')
   reg = /(\r\n\r\n)/g
   json = json.replace(reg, '\r\n')
-  reg = /\r\n,/g
+  reg = /\r\n\,/g
   json = json.replace(reg, ',')
   if (!options.newlineAfterColonIfBeforeBraceOrBracket) {
-    reg = /:\r\n\{/g
+    reg = /\:\r\n\{/g
     json = json.replace(reg, ':{')
-    reg = /:\r\n\[/g
+    reg = /\:\r\n\[/g
     json = json.replace(reg, ':[')
   }
   if (options.spaceAfterColon) {
-    reg = /:/g
+    reg = /\:/g
     json = json.replace(reg, ':')
   }
   (json.split('\r\n')).forEach(function (node, index) {
