2f1b8cd7cbaf6a98bdbf84c41f8264ab918bd34d
a632079 <a632079@qq.com>
2018-04-07 13:55:01 +0800

    Count Host requests

--- src/middlewares/countRequest.js
+++ src/middlewares/countRequest.js
@@ -11,14 +11,15 @@ function countRequest () {
     const fetchRequests = await Promise.all([cache.get('requests'), cache.get('requests:hosts')])
     const all = fetchRequests[0] || 0
     const hosts = fetchRequests[1] || {}
+    const host = String(ctx.request.host)
     cache.set('requests', parseInt(all) + 1)
       .catch(err => {
         winston.error(err)
       })
-    if (!hosts[ctx.request.host]) {
-      hosts[ctx.request.host] = 0
+    if (!hosts[host]) {
+      hosts[host] = 0
     }
-    cache.set('requests:hosts', parseInt(hosts[ctx.request.host]) + 1)
+    cache.set('requests:hosts', parseInt(hosts[host]) + 1)
       .catch(err => {
         winston.error(err)
       })
