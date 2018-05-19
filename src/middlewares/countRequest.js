54ba93ee3fb563ed1d8876a7bf77c0ea6002f451
a632079 <a632079@qq.com>
2018-05-19 10:38:21 +0800

    Update Status

--- src/middlewares/countRequest.js
+++ src/middlewares/countRequest.js
@@ -14,7 +14,7 @@ function countRequest () {
     }
     const host = String(ctx.request.host)
     global.requests.all++
-    global.requests.hosts[host] = typeof global.requests.hosts[host] === 'undefined' ? 0 : parseInt(global.requests.hosts[host]) + 1
+    global.requests.hosts[host] = typeof global.requests.hosts[host] === 'undefined' || global.requests.hosts[host] === null ? 0 : parseInt(global.requests.hosts[host]) + 1
 
     Promise.all([
       cache.set('requests', global.requests.all),
