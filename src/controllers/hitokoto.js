add32bf8d1be5ede071c174f5f9ada4de5096f5f
a632079 <a632079@qq.com>
2020-05-16 19:57:42 +0800

    feat(hitokoto): support length and redis based data collection

--- src/controllers/hitokoto.js
+++ src/controllers/hitokoto.js
@@ -1,159 +1,108 @@
 // Import necessary packages
-const path = require('path')
-const SrcDir = path.join('../../', './src/')
-const db = require(SrcDir + 'db')
 const fastJson = require('fast-json-stringify')
+const cache = require('../cache')
 
 // define fastJson scheme
-const hitokotoFormat = fastJson({
-  title: 'hitokoto',
-  type: 'object',
-  properties: {
-    id: { type: 'integer' },
-    hitokoto: { type: 'string' },
-    type: { type: 'string' },
-    from: { type: 'string' },
-    from_who: { type: 'string', 'nullable': true },
-    creator: { type: 'string' },
-    creator_uid: { type: 'integer' },
-    reviewer: { type: 'integer' },
-    uuid: { type: 'string' },
-    created_at: { type: 'string' }
-  }
-})
 const rawString = fastJson({
   title: 'rawString',
   type: 'string'
 })
-
-let Hitokoto
-let updateLock = false
-
-function sleep (sec) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, sec * 1000)
-  })
+const categories = {
+  collection: [],
+  lastupdated: 0,
+  updateLock: false
 }
 
-async function syncHitokotoList () {
-  if (updateLock) {
-    for (let loop = 1; loop <= 4; loop++) {
-      await sleep(0.5)
-      if (Hitokoto) {
-        return
-      }
-    }
-  }
-  updateLock = true
-  const result = {}
-  const hitokoto = await db.registerModel('hitokoto')
-  // Fetch All Data
-  result.all = await hitokoto.findAll({
-    attributes: {
-      exclude: ['assessor', 'owner']
-    }
-  })
-  // Generate Categroy List
-  result.categroy = {}
-  const categroy = []
-  for (let sentence of result.all) {
-    if (!result.categroy[sentence.type]) {
-      // Init Categroy List
-      result.categroy[sentence.type] = []
-      categroy.push(sentence.type)
-    }
-    result.categroy[sentence.type].push(sentence)
-  }
-  // fill TS
-  result.lastUpdate = Date.now()
-
-  // let Status know
-  global.hitokoto = {}
-  global.hitokoto.total = result.all.length
-  global.hitokoto.categroy = categroy
-  global.hitokoto.lastUpdated = result.lastUpdate
-
-  // Update Data
-  Hitokoto = result
-
-  updateLock = false
-}
 async function hitokoto (ctx, next) {
-  // judge whether data is exist
-  if (!Hitokoto) {
-    // Sync Data
-    await syncHitokotoList()
-  } else if ((Date.now() - Hitokoto.lastUpdate) > 1000 * 60 * 60 * 2) {
-    // Data is outdate. async update.
-    syncHitokotoList()
+  // check categories
+  if (!categories.updateLock && (categories.lastupdated === 0 || (Date.now() - categories.lastupdated) >= 1000 * 60 * 90)) {
+    try {
+      categories.updateLock = true
+      const tmp = await cache.get('hitokoto:bundle:categories')
+      for (const categroy of tmp) {
+        categories.collection.push(categroy.key)
+      }
+      categories.lastupdated = Date.now()
+      categories.updateLock = false
+    } catch (e) {
+      categories.updateLock = false
+      throw e
+    }
   }
+  let sentence = ''
   if (ctx.query && ctx.query.c) {
     // exist params c
     let categroy = ctx.query.c
-
     // support ?c=a&c=b&c=d
     if (Array.isArray(categroy) && categroy.length > 0) {
       categroy = categroy[Math.floor(Math.random() * categroy.length)]
     }
-
-    if (!Hitokoto.categroy[categroy]) {
+    if (!categories.collection.includes(categroy)) {
       ctx.status = 404
       ctx.body = {
         status: 404,
-        message: '很抱歉，该分类下尚无条目'
+        message: '很抱歉，此分类不存在。'
+      }
+      return
+    }
+    // parse length
+    const minLength = ctx.query.min_length || 0
+    const maxLength = ctx.query.max_length && ctx.query.max_length <= 1000 && ctx.query.max_length > minLength ? ctx.query.max_length : 30
+    // get hitokoto sentences id
+    const client = cache.getClient()
+    const uuids = await client.zrangebyscoreAsync('hitokoto:bundle:category:' + categroy, minLength, maxLength)
+    if (uuids.length === 0) {
+      ctx.status = 404
+      ctx.body = {
+        status: 404,
+        message: '很抱歉，没有句子符合长度区间。'
       }
       return
     }
     // Random Sentence
-    const sentence = Hitokoto.categroy[categroy][Math.floor(Math.random() * Hitokoto.categroy[categroy].length)]
+    sentence = await cache.get('hitokoto:sentence:' + uuids[Math.floor(Math.random() * uuids.length)], false)
     // CheckEncoding
-    const encode = ctx.query.encode
-    switch (encode) {
-      case 'json':
-        ctx.type = 'application/json'
-        ctx.body = hitokotoFormat(sentence)
-        break
-      case 'text':
-        ctx.type = 'text/plain'
-        ctx.body = sentence.hitokoto
-        break
-      case 'js':
-        const select = ctx.query.select ? ctx.query.select : '.hitokoto'
-        const response = `(function hitokoto(){var hitokoto=${rawString(sentence.hitokoto)};var dom=document.querySelector('${select}');Array.isArray(dom)?dom[0].innerText=hitokoto:dom.innerText=hitokoto;})()`
-        ctx.type = 'text/javascript'
-        ctx.body = response
-        break
-      default:
-        ctx.type = 'application/json'
-        ctx.body = hitokotoFormat(sentence)
-        break
-    }
   } else {
     // Not Params or just has callback
-    // Random Sentence
-    const sentence = Hitokoto.all[Math.floor(Math.random() * Hitokoto.all.length)]
-    // CheckEncoding
-    const encode = ctx.query.encode
-    switch (encode) {
-      case 'json':
-        ctx.type = 'application/json'
-        ctx.body = hitokotoFormat(sentence)
-        break
-      case 'text':
-        ctx.type = 'text/plain'
-        ctx.body = sentence.hitokoto
-        break
-      case 'js':
-        const select = ctx.query.select ? ctx.query.select : '.hitokoto'
-        const response = `(function hitokoto(){var hitokoto=${rawString(sentence.hitokoto)};var dom=document.querySelector('${select}');Array.isArray(dom)?dom[0].innerText=hitokoto:dom.innerText=hitokoto;})()`
-        ctx.type = 'text/javascript'
-        ctx.body = response
-        break
-      default:
-        ctx.type = 'application/json'
-        ctx.body = hitokotoFormat(sentence)
-        break
+    const categroy = categories.collection[Math.floor(Math.random() * categories.collection.length)]
+    console.log(categroy)
+    // parse length
+    const minLength = ctx.query.min_length || 0
+    const maxLength = ctx.query.max_length && ctx.query.max_length <= 1000 && ctx.query.max_length > minLength ? ctx.query.max_length : 30
+    // get hitokoto sentences id
+    const client = cache.getClient()
+    const uuids = await client.zrangebyscoreAsync('hitokoto:bundle:category:' + categroy, minLength, maxLength)
+    if (uuids.length === 0) {
+      ctx.status = 404
+      ctx.body = {
+        status: 404,
+        message: '很抱歉，没有句子符合长度区间。'
+      }
+      return
     }
+    // Random Sentence
+    sentence = await cache.get('hitokoto:sentence:' + uuids[Math.floor(Math.random() * uuids.length)], false)
+  }
+  const encode = ctx.query.encode
+  switch (encode) {
+    case 'json':
+      ctx.type = 'application/json'
+      ctx.body = sentence
+      break
+    case 'text':
+      ctx.type = 'text/plain'
+      ctx.body = sentence.hitokoto
+      break
+    case 'js':
+      const select = ctx.query.select ? ctx.query.select : '.hitokoto'
+      const response = `(function hitokoto(){var hitokoto=${rawString(sentence.hitokoto)};var dom=document.querySelector('${select}');Array.isArray(dom)?dom[0].innerText=hitokoto:dom.innerText=hitokoto;})()`
+      ctx.type = 'text/javascript'
+      ctx.body = response
+      break
+    default:
+      ctx.type = 'application/json'
+      ctx.body = sentence
+      break
   }
 }
 module.exports = hitokoto
