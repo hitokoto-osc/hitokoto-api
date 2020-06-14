9d83a9282cb98cd8e95ce2ab8fca4472b1a5b65d
a632079 <a632079@qq.com>
2020-06-14 12:56:38 +0800

    chore(eslint): use definate parseOptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
-  extends: "standard",
+  extends: 'standard',
+  parserOptions: {
+    ecmaVersion: '2020',
+    sourceType: 'script',
+    ecmaFeatures: {
+      impliedStrict: true
+    }
+  },
   env: {
     node: true
   },
   overrides: [
     {
       files: [
-        "**/*.test.js"
+        '**/*.test.js'
       ],
       env: {
-        jest: true//now **/*.test.js files' env has both es6 *and* jest
+        jest: true //now **/*.test.js files' env has both es6 *and* jest
       },
       // Can't extend in overrides: https://github.com/eslint/eslint/issues/8813
       //"extends": ["plugin:jest/recommended"]
