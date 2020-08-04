97a99aaa7d1b963e15e107b158a1cae4d6894074
a632079 <i@a632079.me>
2020-08-05 03:18:02 +0800

    feat(controllers): generate controllers map including sub-dir

--- src/controller.js
+++ src/controller.js
@@ -4,6 +4,40 @@ const winston = require('winston')
 const colors = require('colors/safe')
 const path = require('path')
 
+function readDir (dir) {
+  const files = fs.readdirSync(dir)
+  if (files.length <= 0) return
+  const map = {}
+  for (const file of files) {
+    const filePath = path.join(dir, file)
+    const stat = fs.statSync(filePath)
+    if (stat.isDirectory()) {
+      const subDirMaps = readDir(filePath)
+      if (subDirMaps && subDirMaps.length > 0) {
+        map[file] = map[file]
+          ? Object.assign(
+            map[file],
+            subDirMaps
+          )
+          : subDirMaps
+      }
+    } else {
+      map[file.substring(0, file.length - 3)] = map[file.substring(0, file.length - 3)]
+        ? Object.assign(
+          map[file.substring(0, file.length - 3)],
+          require(filePath)
+        )
+        : require(filePath)
+    }
+  }
+  return map
+}
+
+function genControllersMap () {
+  const dir = path.join(__dirname, './controllers')
+  return readDir(dir)
+}
+
 class controllers {
   constructor (controller) {
     if (controller) {
@@ -28,11 +62,7 @@ class controllers {
   async load () {
     try {
       // Load Controller
-      const controllers = {}
-      const dir = fs.readdirSync(path.join(__dirname, '../', './src/controllers'))
-      await dir.map((item, index, input) => {
-        controllers[item.substring(0, item.length - 3)] = module.parent.require(path.join(__dirname, '../', './src/controllers/' + item))
-      })
+      const controllers = genControllersMap()
       return controllers
     } catch (e) {
       winston.error(colors.red(e))
