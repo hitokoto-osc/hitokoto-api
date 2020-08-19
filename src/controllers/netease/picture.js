09cece621cc26c1bd8103fddcba26dbf07f860aa
a632079 <i@a632079.me>
2020-08-19 16:46:56 +0800

    refactor(ncm): picture

--- src/controllers/netease/picture.js
+++ src/controllers/netease/picture.js
@@ -1,39 +1,12 @@
 // This module is intended to get picture url
 const Joi = require('joi')
-const crypto = require('crypto')
+const { getPictureURL } = require('./_sdk_wrapper')
 // validation schema
 const { ValidateParams } = require('../../utils/response')
 const schema = Joi.object({
   id: Joi.string().required(),
-  height: Joi.string().default(300),
+  height: Joi.number().default(300),
 })
-
-const md5 = (data) => {
-  const buf = Buffer.from(data)
-  const str = buf.toString('binary')
-  return crypto.createHash('md5').update(str).digest('base64')
-}
-
-const neteasePickey = (id) => {
-  id = String(id)
-  const magic = '3go8&$8*3*3h0k(2)2'.split('')
-  const songId = id
-    .split('')
-    .map((item, index) =>
-      String.fromCharCode(
-        item.charCodeAt(0) ^ magic[index % magic.length].charCodeAt(0),
-      ),
-    )
-  return md5(songId.join('')).replace(/\//g, '_').replace(/\+/g, '-')
-}
-
-const picture = (id, size = 300) => {
-  return Promise.resolve({
-    url: `https://p3.music.126.net/${neteasePickey(
-      id,
-    )}/${id}.jpg?param=${size}y${size}`,
-  })
-}
 
 module.exports = async (ctx) => {
   const params = Object.assign({}, ctx.params, ctx.query, ctx.request.body)
@@ -41,6 +14,8 @@ module.exports = async (ctx) => {
     // validateParams
     return
   }
-  const pictureURL = await picture(params.id, params.height)
-  ctx.body = pictureURL
+  const pictureURL = getPictureURL(params.id, params.height)
+  ctx.body = {
+    url: pictureURL,
+  }
 }
