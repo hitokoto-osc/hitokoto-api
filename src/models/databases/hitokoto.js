24594ebea40056bffa7dc5ccdbd426828dc4086a
a632079 <i@a632079.me>
2020-09-29 17:28:54 +0800

    fix(lint): database model hitokoto

--- src/models/databases/hitokoto.js
+++ src/models/databases/hitokoto.js
@@ -2,61 +2,64 @@
 const model = require('./model')
 
 // Define Database Model
-module.exports = [{
-  id: {
-    type: model.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+module.exports = [
+  {
+    id: {
+      type: model.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    hitokoto: {
+      type: model.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: model.STRING,
+      allowNull: false,
+    },
+    from: {
+      type: model.STRING,
+      allowNull: false,
+    },
+    from_who: {
+      type: model.STRING,
+      allowNull: true,
+    },
+    creator: {
+      type: model.STRING,
+      allowNull: false,
+      defaultValue: 'hitokoto',
+    },
+    creator_uid: {
+      type: model.STRING,
+      allowNull: true,
+    },
+    assessor: {
+      type: model.STRING,
+      allowNull: true,
+    },
+    owner: {
+      type: model.STRING,
+      allowNull: false,
+    },
+    reviewer: {
+      type: model.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    uuid: {
+      type: model.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: model.STRING,
+      allowNull: false,
+    },
   },
-  hitokoto: {
-    type: model.STRING,
-    allowNull: false
+  {
+    // Sequelize Model config
+    timestamps: false,
+    freezeTableName: true,
+    tableName: 'hitokoto_sentence',
   },
-  type: {
-    type: model.STRING,
-    allowNull: false
-  },
-  from: {
-    type: model.STRING,
-    allowNull: false
-  },
-  from_who: {
-    type: model.STRING,
-    allowNull: true
-  },
-  creator: {
-    type: model.STRING,
-    allowNull: false,
-    defaultValue: 'hitokoto'
-  },
-  creator_uid: {
-    type: model.STRING,
-    allowNull: true
-  },
-  assessor: {
-    type: model.STRING,
-    allowNull: true
-  },
-  owner: {
-    type: model.STRING,
-    allowNull: false
-  },
-  reviewer: {
-    type: model.INTEGER,
-    allowNull: false,
-    defaultValue: 1
-  },
-  uuid: {
-    type: model.STRING,
-    allowNull: false
-  },
-  created_at: {
-    type: model.STRING,
-    allowNull: false
-  }
-}, {
-  // Sequelize Model config
-  timestamps: false,
-  freezeTableName: true,
-  tableName: 'hitokoto_sentence'
-}]
+]
