05e86fc390bbda340977be2eab21b5dbbdee6621
a632079 <i@a632079.me>
2020-10-06 13:30:25 +0800

    feat: pm2 startup script

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: '@hitokoto/api.v1',
+      script: './core.js',
+      watch: true,
+      interpreter: 'yarn', // absolute path to yarn ; default is node
+      interpreter_args: 'start',
+      exec_mode: 'fork',
+      cwd: '', // the directory from which your app will be launched
+      args: '', // string containing all arguments passed via CLI to script
+      env_production: {
+        PORT: 8000,
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
