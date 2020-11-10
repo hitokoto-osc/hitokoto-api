da6ecf703cee1ac9f3b3ba4098755e28769ef10c
a632079 <i@a632079.me>
2020-11-10 19:56:24 +0800

    chore: add alinode config

---
+++ ecosystem.alinode.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: '@hitokoto/api.v1',
+      script: './core.js',
+      watch: true,
+      ignore_watch: ['./data/logs'],
+      interpreter: 'yarn', // absolute path to yarn ; default is node
+      interpreter_args: 'start',
+      exec_mode: 'fork',
+      cwd: '', // the directory from which your app will be launched
+      args: '', // string containing all arguments passed via CLI to script
+      env_production: {
+        PORT: 8000,
+        NODE_ENV: 'production',
+        ENABLE_NODE_LOG: 'YES',
+        NODE_LOG_DIR: '~/alinode_logs',
+      },
+    },
+  ],
+}
