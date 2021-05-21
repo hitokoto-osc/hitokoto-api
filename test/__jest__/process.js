f27d41b9b5e7c3d4071a9ff5b4b8ded8fcb2e109
greenhat616 <i@a632079.me>
2021-05-21 20:47:59 +0800

    test: enable core test stderr

--- test/__jest__/process.js
+++ test/__jest__/process.js
@@ -15,7 +15,7 @@ module.exports = class Process {
             'redis.host': '127.0.0.1',
             'redis.port': 6379,
           },
-          stdio: ['ipc', 'ignore', 'pipe'],
+          stdio: ['ipc', 'ignore', process.stderr],
         })
         core.on('exit', (code, signal) => {
           if (code === 0 || signal === 'SIGTERM') {
