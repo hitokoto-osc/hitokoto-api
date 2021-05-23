2b446191f453dc5c686f9fa88645ccadc775b5a5
greenhat616 <i@a632079.me>
2021-05-23 15:41:50 +0800

    fix(tracing): unexpected version

--- src/tracing.js
+++ src/tracing.js
@@ -1,10 +1,10 @@
 const Sentry = require('@sentry/node')
 const Tracing = require('@sentry/tracing')
-
+const nconf = require('nconf')
 // Init Sentry Server
 Sentry.init({
   dsn: 'https://9e3019aa154c45ba8affa9a34c7fe162@o673612.ingest.sentry.io/5768310',
-  release: 'hitokoto-api@' + process.env.npm_package_version,
+  release: 'hitokoto-api@v' + nconf.get('version'),
   tracesSampleRate: 0.001,
 })
 
